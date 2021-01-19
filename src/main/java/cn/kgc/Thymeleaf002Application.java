19ae11966254ab4615ada601a00c7e28059bade4
86132 <2863693864@qq.com>
2021-01-19 16:03:07 +0800

    1.0 版本

---
+++ src/main/java/cn/kgc/Thymeleaf002Application.java
@@ -0,0 +1,13 @@
+package cn.kgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thymeleaf002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Thymeleaf002Application.class, args);
+    }
+
+}
